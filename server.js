a252243b094b31bd884441cf41cafe8736b01917
jayaram47 <jayaram47puli@gmail.com>
2020-01-02 11:06:53 +0530

    updated2

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const path = required('path')
+const path = require('path')
 const app = express()
 const port = process.env.PORT||3000
 app.use(express.static(__dirname + '/dist/ngproject'));
